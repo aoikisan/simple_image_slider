ceeece612534f869bdf1831062a594cccca5e2b2
aoikisan <127423644+aoikisan@users.noreply.github.com>
2023-03-19 11:46:15 -0500

    Create slider.js

---
+++ slider.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  var slideIndex = 1;
+  showSlide(slideIndex);
+  
+  $(".slider-prev").click(function() {
+    showSlide(slideIndex -= 1);
+  });
+  
+  $(".slider-next").click(function() {
+    showSlide(slideIndex += 1);
+  });
+  
+  function showSlide(n) {
+    var i;
+    var slides = $(".slider-images img");
+    var texts = $(".slider-text");
+    
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    
+    for (i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+    }
+    slides[slideIndex-1].classList.add("active");
+    
+    texts.html("<h2>" + slides[slideIndex-1].alt + "</h2><p>Contextual subtext about the image.</p>");
+  }
+});
